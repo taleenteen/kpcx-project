9f20371d446ec0c3cb7951e02b5e356393956372
taleenteen <teenteenza1@hotmail.com>
2024-08-30 01:46:30 +0700

    add padding at contact card

--- app/components/SectionSeven/Contact.tsx
+++ app/components/SectionSeven/Contact.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
         style={{ backgroundImage: `url(${img})` }}
       >
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="text-center text-white">
+          <div className="text-center text-white p-4">
             <h1 className="text-4xl font-bold mb-4">
               Ready to elevate your business?
             </h1>
